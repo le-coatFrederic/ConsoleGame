d86374c83ea7c62b5fa08ab1c77b63c3283733d4
BuildTools <unconfigured@null.spigotmc.org>
2022-10-23 00:45:39 +0200

    added items

---
+++ includes/items/items.hpp
@@ -0,0 +1,22 @@
+#ifndef ITEM
+#define ITEM
+
+#include <string>
+
+class Item {
+protected:
+    int id;
+    std::string name;
+public:
+    int getId();
+    std::string getName();
+};
+
+class UsabledItem : public Item {
+protected:
+    int durability;
+public:
+    virtual int getDurability();
+};
+
+#endif
